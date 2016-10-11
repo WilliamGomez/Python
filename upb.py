d9253c962e5d572af149419ab2c196502494e0c8
William Gomez <williamgomez346@gmail.com>
2016-10-11 12:43:15 -0500

    Create upb.py

---
+++ upb.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- coding: Windows-1252 -*-
+# -*- coding: cp1252 -*-
+# -*- coding: IBM850 -*-
+# -*- coding: 850 -*-
+# -*- coding: cp850 -*-
+#
+# IMPORTS
+#
+import os
+import sys
+#
+#
+#   Trabajo Funciones
+#
+print sys.stdout.encoding
+if "linux" in sys.platform:
+  os.system("clear")
+elif "win" in sys.platform:
+  os.system("cls")
+else:
+  pass
+
+_author_ = "David Leon"
+_version_= "1.0"
+
+#Colores
+color = {"blue": "\033[94m", "red": "\033[91m", "green": "\033[92m", "white": "\033[0m", "yellow": "\033[93m"}
+
+                                                             
+                                                             
+print color['white'] + "                                                             " + color['white']
+print color['white'] + "                 @+'''''''''''''''''''''''''+@               " + color['white']
+print color['white'] + "                  @+'''''''''''''''''''''##                  " + color['white']
+print color['white'] + "                     ##'''''''''''''''''#+                   " + color['white']
+print color['white'] + "                       +#'''''''''''''@'                     " + color['white']
+print color['white'] + "                         '@'''''''''@:                       " + color['white']
+print color['white'] + "                           @''';'''@                         " + color['white']
+print color['white'] + "                           @'':,;''@                         " + color['white']
+print color['white'] + "                           @'':,;''@                         " + color['white']
+print color['white'] + "                           @'':,;''@                         " + color['white']
+print color['white'] + "                ###########@@@@@@@@@###########              " + color['white']
+print color['white'] + "                :,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'              " + color['white']
+print color['white'] + "                :,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'              " + color['white']
+print color['white'] + "                :,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'              " + color['white']
+print color['white'] + "      @         :,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'         @    " + color['white']
+print color['white'] + "      @,        :,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'        '@    " + color['white']
+print color['white'] + "      @@        :,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'        @@    " + color['white']
+print color['white'] + "      @'.       :,,,@@@@@:@@@@@@@@@@,#:#@@@,,,'       ;'@    " + color['white']
+print color['white'] + "      @'@       :,,,@@@@@,@@@@@@@@@,@@@,@@@,,,'       @'@    " + color['white']
+print color['white'] + "      @''`      :,,,@@@@,++@@@@@@@@:@@@:@@@,,,'      ,''@    " + color['white']
+print color['white'] + "      @''@      :,,,@@@@##,@@@@@@@@,@@@,@@@,,,'      @''@    " + color['white']
+print color['white'] + "      @'''      :,,,@@@:@@;@@@@@@@#@:@:@+@@,,,'     .'''@    " + color['white']
+print color['white'] + "      @'''@     :,,,@@+:@@',@@@@@@#;:@:;+@@,,,'     @'''@    " + color['white']
+print color['white'] + "      @'''+     :,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'    `''''@    " + color['white']
+print color['white'] + "      @''''@....:,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'....@''''@    " + color['white']
+print color['white'] + "      @''''''''':,,,@@@@@@@@@@@'@@@@@@@@@@@,,,''''''''''@    " + color['white']
+print color['white'] + "      @''''''''':,,,@@@@@@@@@@@'+@@@@@@@@@@,,,''''''''''@    " + color['white']
+print color['white'] + "      @''''@@@@@:,,,@@@@@@@@@@@:+'@@@@@@@@@,,,'@@@@#''''@    " + color['white']
+print color['white'] + "      @''''@,,,,:,,,@@@@@@@@@@#++:@@@@@@@@@,,,',,,,#''''@    " + color['white']
+print color['white'] + "      @''''@,,,,:,,,@@@@@@@@@@;++:@@@@@@@@@,,,',,,,#''''@    " + color['white']
+print color['white'] + "      @''''#####:,,,@@@@@@@@@@;;+'@@@@@@@@@,,,'####+''''@    " + color['white']
+print color['white'] + "      @''''''''':,,,@@@@@@@@@@@@@@@@@@@@@@@,,,''''''''''@    " + color['white']
+print color['white'] + "      @''''+++++:,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'++++'''''@    " + color['white']
+print color['white'] + "      @'''''    :,,,@@:;@@;+@@;;;@@@#;;;@@@,,,'    @''''@    " + color['white']
+print color['white'] + "      @'''@     :,,,@@;@@@;@@@@;@,@@@,@@,@@,,,'     #'''@    " + color['white']
+print color['white'] + "      @''';     :,,,@@;@@@;@@@@;@,@@@,@@,@@,,,'     #'''@    " + color['white']
+print color['white'] + "      @''@      :,,,@@;@@@;@@@@;+@@@@,@@,@@,,,'      #''@    " + color['white']
+print color['white'] + "      @'',      :,,,@@'@@@:@@@@;@@@@@,@@:@@,,,'      '''@    " + color['white']
+print color['white'] + "      @'@       :,,,@@@,#;@@@@+,@@@@@,';;@@,,,'       @'@    " + color['white']
+print color['white'] + "      @'.       :,,,@@@@@@@@@@@@@@@@@@@@@@@,,,'       ;'@    " + color['white']
+print color['white'] + "      @@        ;,,,@@@@@@@@@@@@@@@@@@@@@@@,,,+        @@    " + color['white']
+print color['white'] + "      @`        @,,,#@@@@@@@@@@@@@@@@@@@@@#,,,@        ,@    " + color['white']
+print color['white'] + "      +         `;,,,:''''''''#@#'''''''':,,,'          '    " + color['white']
+print color['white'] + "                 ;:,,,,,,,,,,,,;,,,,,,,,,,,,;,               " + color['white']
+print color['white'] + "                  ,+,,,,,,,,,,,,,,,,,,,,,,,#.                " + color['white']
+print color['white'] + "                    @+;;;;;;;;:,:;;;;;;;;#@                  " + color['white']
+print color['white'] + "                           @'';@;''@                         " + color['white']
+print color['white'] + "                           @'':,;''@                         " + color['white']
+print color['white'] + "                           @'':,;''@                         " + color['white']
+print color['white'] + "                           @'':,;''@                         " + color['white']
+print color['white'] + "                          :@''@@@''@,                        " + color['white']
+print color['white'] + "                        '@'''''''''''@:                      " + color['white']
+print color['white'] + "                      +#'''''''''''''''@'                    " + color['white']
+print color['white'] + "                    ##'''''''''''''''''''#+                  " + color['white']
+print color['white'] + "                  @+'''''''''''''''''''''''##                " + color['white']
+print color['white'] + "                .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               " + color['white']
+print color['white'] + "                                                             " + color['white']
+print color['white'] + "#############################################################" + color['white']
+print color['white'] + "#                TRABAJO FUNCIONES Y ARREGLOS               #" + color['white']
+print color['red'] +   "#               Desarrollador por David Leon                #" + color['red']
+print color['white'] + "#############################################################" + color['white']
+
+## Start Menu Python
+      
+def print_menu():       ## Your menu design here
+    print 30 * "-" , "MENU" , 30 * "-"
+    print u"1. Calcular el promedio de los números de una lista."
+    print u"2. Convertir los números negativos en positivos dentro de una lista."
+    print u"3. Sumar dos vectores del mismo tamaño."
+    print u"4. Comparar dos vectores, Iguales=Verdadero, Diferente=Falso."
+    print u"5. Salir"
+    print 67 * "-"
+
+  
+loop=True      
+  
+while loop:          ## While loop which will keep going until loop = False
+    print_menu()    ## Displays menu
+    choice = raw_input("Enter your choice [1-5]: ")
+     
+    if choice=="1":     
+        print u"Selecciono la opcion 1."
+        i=int(1)
+        vect=[none]
+        vect=[0]=0
+        tamvector=int(input("Diga el tamaño del vector"))
+        while ( i < tamvector):
+          carga=int(input"Ingrese el", i, "del vector")
+          vect=[i]=carga
+          i=i+1
+        print vect
+        ## You can add your code or functions here
+    elif choice=="2":
+        print u"Selecciono la opcion 2."
+        ## You can add your code or functions here
+    elif choice=="3":
+        print u"Selecciono la opcion 3."
+        ## You can add your code or functions here
+    elif choice=="4":
+        print u"Selecciono la opcion 4."
+        ## You can add your code or functions here
+    elif choice=="5":
+        print u"Selecciono la opcion 5."
+        ## You can add your code or functions here
+        print u"Saliendo del Algoritmo"
+        loop=False # This will make the while loop to end as not value of loop is set to False
+    else:
+        # Any integer inputs other than values 1-5 we print an error message
+        print "No selecciono ninguna opcion"
+        print "Saliendo del Algoritmo"
+        print color['white'] + "#############################################################" + color['white']
+        print color['white'] + "#                       FIN ALGORITMO                       #" + color['white']
+        print color['red'] +   "#               Desarrollador por David Leon                #" + color['red']
+        print color['white'] + "#############################################################" + color['white']
+        loop=False
